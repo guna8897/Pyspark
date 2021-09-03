594b0c993d9b2247ae8ca3a124dd830bd4a313cd
guna8897 <iamseelan@gmail.com>
2021-09-03 17:32:15 +0530

    Day2

---
+++ Python/Day 2/lists.py
@@ -0,0 +1,6 @@
+animals = ['king lion','tiger','cheetah','rabbit']
+animal = animals[1]
+print(animal.title())
+for index, animal in enumerate(animals):
+    place = str(index)
+print(place + animal.title())
